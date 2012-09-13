3d23213f71727ac9aa16c68961a2db20e3df0b1c
Adam Thornburn <adam.thornburn@defize.co.uk>
2012-09-13 23:52:47 +0100

    Moved JabbR client to separate project.

--- Src/CompileThis.BawBag.Core/Jabbr/JabbrRoomSummary.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CompileThis.BawBag.Jabbr
-{
-    public class JabbrRoomSummary
-    {
-        public string Name { get; set; }
-        public bool Private { get; set; }
-        public bool Closed { get; set; }
-    }
-}
---
+++ Src/CompileThis.BawBag.Jabbr/ActionReceivedEventArgs.cs
@@ -0,0 +1,33 @@
+﻿namespace CompileThis.BawBag.Jabbr
+{
+    using System;
+
+    public class ActionReceivedEventArgs : EventArgs
+    {
+        private readonly JabbrUser _user;
+        private readonly JabbrRoom _room;
+        private readonly string _actionText;
+
+        public ActionReceivedEventArgs(JabbrUser user, JabbrRoom room, string actionText)
+        {
+            _user = user;
+            _room = room;
+            _actionText = actionText;
+        }
+
+        public JabbrUser User
+        {
+            get { return _user; }
+        }
+
+        public JabbrRoom Room
+        {
+            get { return _room; }
+        }
+
+        public string ActionText
+        {
+            get { return _actionText; }
+        }
+    }
+}
