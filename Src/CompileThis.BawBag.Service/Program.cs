594aa3532f3bcadbf1e5af2b733527f4f5b9dc55
Adam Thornburn <adam.thornburn@defize.co.uk>
2013-05-13 21:18:11 +0100

    Add unhandled exception logger

--- Src/CompileThis.BawBag.Service/Program.cs
+++ Src/CompileThis.BawBag.Service/Program.cs
@@ -1,13 +1,19 @@
 ﻿namespace CompileThis.BawBag.Service
 {
     using CompileThis.BawBag.Service.Runtime;
+using System;
+
+    using NLog;
 
     using Topshelf;
 
     internal class Program
     {
+        private static readonly Logger Log = LogManager.GetCurrentClassLogger();
+
         public static void Main()
         {
+            AppDomain.CurrentDomain.UnhandledException += UnhandledExceptionProcessor;
             HostFactory.Run(hostConfig =>
                 {
                     hostConfig.SetServiceName("BawBag");
@@ -23,6 +29,12 @@
                         serviceConfig.ConstructUsing(hs => new BawBagService());
                     });
                 });
+        }
+
+        private static void UnhandledExceptionProcessor(object sender, UnhandledExceptionEventArgs e)
+        {
+            Log.FatalException("Unhandled exception", e.ExceptionObject as Exception);
+            Environment.Exit(256);
         }
     }
 }
